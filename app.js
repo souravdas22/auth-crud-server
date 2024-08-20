8504d879af864770bceafd39cf9f647b950e5f00
Coolsourav <9163905324das@gmail.com>
2024-08-20 12:15:47 +0530

    project finalised

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use("/api", userApiRouter);
 app.use("/api", ProductApiRouter);
 app.use("/api", testRoute);
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 7000 ;
 
 app.listen(PORT, () =>
   console.log(`App is listening on port http://localhost:${PORT}`)
